643be3fa1f47810c254116488312c05216c34cb2
Joseph <Slugslingermario@gmail.com>
2024-09-18 17:47:09 +0800

    Added styles, small optimization
    - Nested focus action listener
    - Attempt at handling empty spaces in text fields

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const myModal = document.getElementById('mymodal')
-const myInput = document.getElementById('tfTaskName')
 const createTaskBtn= document.getElementById('mrbutton')
 const tBody = document.getElementById('table-body')
 const addRowBtn = document.getElementById('addRowBtn')
 
 let counter = 0;
 
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
+createTaskBtn.addEventListener('click', () => {
+  const myInput = document.getElementById('tfTaskName-1')
+  myModal.addEventListener('shown.bs.modal', () => {
+    myInput.focus()
+  })
 })
 
 function highlightRow(id){
@@ -15,33 +17,50 @@ function highlightRow(id){
   console.log("checked box " + id)
 }
 
+// function highlightEmpty(tfEmpty){
+//   tfEmpty.
+// }
+
 addRowBtn.addEventListener('click', () => {
+  let err = false;
   console.log("Button Clicked");
 
   const details = document.getElementsByClassName("tfCreateTask");
   
   var row = tBody.insertRow();
   row.id = "table-row-" + counter;
-  counter++;
 
   for (let i = 0; i < details.length; i++) {
-    var cell = row.insertCell();
     var text = details.item(i).value;
-    console.log(text);
+
+    //check if empty, idk about spaces
+    if(text === ""){
+      console.log("Empty text detected at Text Field ID: " + details.item(i).id);
+      // highlightEmpty(details.item(i));
+      err = true
+      break
+    }
+
+    console.log(text)
+
+    var cell = row.insertCell();
 
     var textNode = document.createTextNode(text); 
     cell.appendChild(textNode);
   }
 
-  var cell = row.insertCell();
+  if(err){
+    return null;
+  }
 
+ var cell = row.insertCell();
+
+  // add checkbox
   var checkBox = document.createElement("input");
   checkBox.type = "checkbox";
-  checkBox.classList.add("finishedCheck");
-
-  var parentRowId = checkBox.parentNode.id;
-
-  checkBox.onclick = function() {highlightRow(parentRowId)};
+  checkBox.classList.add("cbFinished-" + counter);
 
   cell.appendChild(checkBox);
+  counter++;
 })
+
